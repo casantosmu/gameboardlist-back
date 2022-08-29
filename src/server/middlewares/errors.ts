a832afa04b4e421d97737c46709d2fb9e4024fed
Carlos <casantosmu@gmail.com>
2022-08-29 18:12:29 +0200

    Addded notFoundError and generalError middlewares

---
+++ src/server/middlewares/errors.ts
@@ -0,0 +1,26 @@
+import chalk from "chalk";
+import Debug from "debug";
+import { NextFunction, Request, Response } from "express";
+import CustomError from "../../utils/CustomError";
+
+const debug = Debug("gameboardlist:server:errors");
+
+export const notFoundError = (req: Request, res: Response) => {
+  res.status(404).json({ error: "Endpoint not found" });
+};
+
+export const generalError = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction
+) => {
+  const status = error.status ?? 500;
+  const publicMessage = error.publicMessage ?? "General error";
+  const privateMessage = error.privateMessage ?? error.message;
+
+  debug(chalk.red(privateMessage));
+
+  res.status(status).json({ error: publicMessage });
+};
