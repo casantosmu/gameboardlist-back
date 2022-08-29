70ac22b4f5adf83341b98b07c52e704c6cb642cf
Carlos <casantosmu@gmail.com>
2022-08-29 20:28:01 +0200

    Added auth router

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import { generalError, notFoundError } from "./middlewares/errors";
+import authRouter from "./routers/authRouter";
 
 const app = express();
 
@@ -10,9 +11,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.disable("x-powered-by");
 
-app.get("/", (_, res) => {
-  res.send("Hello World!");
-});
+app.use("/auth", authRouter);
 
 app.use(notFoundError);
 app.use(generalError);
