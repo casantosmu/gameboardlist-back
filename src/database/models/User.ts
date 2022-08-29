8896f39db31dc032c032c82be31da96f591ec7fb
Carlos <casantosmu@gmail.com>
2022-08-29 20:29:12 +0200

    Added User model

---
+++ src/database/models/User.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 50,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+});
+
+const User = mongoose.model("User", UserSchema, "users");
+
+export default User;
