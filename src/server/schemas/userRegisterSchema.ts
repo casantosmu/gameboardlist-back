169503d7fdca0da7dc11fc9fcdc2fd1e7343d97c
Carlos <casantosmu@gmail.com>
2022-08-30 20:22:03 +0200

    Added userRegister validation && Modified error middleware

--- src/server/schemas/userRegisterSchema.ts
+++ src/server/schemas/userRegisterSchema.ts
@@ -2,9 +2,11 @@ import { Joi } from "express-validation";
 
 const userRegisterSchema = {
   body: Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().required().min(3).max(50),
-    password: Joi.string().min(6),
+    user: {
+      name: Joi.string().required(),
+      email: Joi.string().required().min(3).max(50),
+      password: Joi.string().min(6),
+    },
   }),
 };
 
