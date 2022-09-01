0e6ecc07366f5a6e3d19eec8abb3d39c7ba55a99
Carlos <casantosmu@gmail.com>
2022-09-01 18:44:09 +0200

    Added userLoginSchema

---
+++ src/server/schemas/userLoginSchema.ts
@@ -0,0 +1,12 @@
+import { Joi } from "express-validation";
+
+const userLoginSchema = {
+  body: Joi.object({
+    user: Joi.object({
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    }).required(),
+  }),
+};
+
+export default userLoginSchema;
