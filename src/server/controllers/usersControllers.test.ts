82ed6c4027a1156fa72478ff41a066ef5d9d04be
Carlos <casantosmu@gmail.com>
2022-09-02 15:52:28 +0200

    Added test when register user calls json and status response methods

--- src/server/controllers/usersControllers.test.ts
+++ src/server/controllers/usersControllers.test.ts
@@ -1,5 +1,5 @@
 import "../../loadEnvironment";
-import { Response } from "express";
+import { Request, Response } from "express";
 import User from "../../database/models/User";
 import {
   CustomRequest,
@@ -54,6 +54,50 @@ describe("Given a registerUser middleware", () => {
         await registerUser(req, res, next);
 
         expect(User.create).toHaveBeenCalledWith(hashedUser);
+      });
+
+      test("Then it should call the response method with a status 201", async () => {
+        const req = {
+          body: {
+            user: {},
+          },
+        } as Partial<Request>;
+        const res = {
+          status: jest.fn().mockReturnThis(),
+          json: jest.fn(),
+        } as Partial<Response>;
+        const next = jest.fn();
+
+        const expectedStatus = 201;
+
+        User.findOne = jest.fn().mockReturnValue(null);
+        User.create = jest.fn().mockReturnValue({});
+
+        await registerUser(req as Request, res as Response, next);
+
+        expect(res.status).toHaveBeenCalledWith(expectedStatus);
+      });
+
+      test("Then it should call the json method with a success message", async () => {
+        const req = {
+          body: {
+            user: {},
+          },
+        } as Partial<Request>;
+        const res = {
+          status: jest.fn().mockReturnThis(),
+          json: jest.fn(),
+        } as Partial<Response>;
+        const next = jest.fn();
+
+        const expectedMessage = { sucess: "User has been registered" };
+
+        User.findOne = jest.fn().mockReturnValue(null);
+        User.create = jest.fn().mockReturnValue({});
+
+        await registerUser(req as Request, res as Response, next);
+
+        expect(res.json).toHaveBeenCalledWith(expectedMessage);
       });
     });
   });
