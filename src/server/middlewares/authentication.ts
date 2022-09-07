88313f0a4629ee1977882dc3319a3f020def3080
Carlos <casantosmu@gmail.com>
2022-09-07 17:35:14 +0200

    Added authauthentication middleware

---
+++ src/server/middlewares/authentication.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from "express";
+import { JwtPayload } from "jsonwebtoken";
+import { verifyToken } from "../../utils/authentication";
+import CustomError from "../../utils/CustomError";
+
+interface CustomRequest extends Request {
+  payload: JwtPayload;
+}
+
+const authentication = (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const authenticationHeader = req.get("Authorization");
+
+  const authenticationError = new CustomError(401, "Invalid authentication");
+
+  if (!authenticationHeader || !authenticationHeader.startsWith("Bearer ")) {
+    next(authenticationError);
+    return;
+  }
+
+  const token = authenticationHeader.split(" ")[1];
+
+  try {
+    const payload = verifyToken(token);
+    req.payload = payload as JwtPayload;
+
+    next();
+  } catch (error) {
+    if (error instanceof Error) {
+      authenticationError.privateMessage = error.message;
+    }
+
+    next(authenticationError);
+  }
+};
+
+export default authentication;
