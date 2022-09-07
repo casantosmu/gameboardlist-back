74bda8d3f1dd8a44fd4f6559957690d820501b1d
Carlos <casantosmu@gmail.com>
2022-09-07 16:13:10 +0200

    Added more validation too userSchema and added array of gameboards

--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -1,19 +1,30 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    trim: true,
   },
   password: {
     type: String,
     required: true,
+    minlength: 6,
   },
+  gameboards: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Gameboard",
+    },
+  ],
 });
 
 const User = mongoose.model("User", userSchema, "users");
