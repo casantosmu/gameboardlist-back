f857ee0de7baa1bd7d77474ad4568a853769e420
Carlos <casantosmu@gmail.com>
2022-09-09 09:24:32 +0200

    Added weight to gameboardSchema

--- src/database/models/Gameboard.ts
+++ src/database/models/Gameboard.ts
@@ -3,12 +3,18 @@ import { model, Schema, Types } from "mongoose";
 const gameboardSchema = new Schema({
   image: {
     type: String,
-    default: "/uploads/example.jpeg",
+    required: true,
   },
   rating: {
     type: Number,
     min: 1,
     max: 10,
+    required: true,
+  },
+  weight: {
+    type: Number,
+    min: 1,
+    max: 5,
     required: true,
   },
   name: {
@@ -27,17 +33,16 @@ const gameboardSchema = new Schema({
     required: true,
   },
   players: {
-    min: { type: Number, min: 1, max: 50 },
-    max: { type: Number, min: 1, max: 50 },
+    min: { type: Number, min: 1, max: 50, required: true },
+    max: { type: Number, min: 1, max: 50, required: true },
   },
   time: {
-    min: { type: Number, min: 1, max: 1200 },
-    max: { type: Number, min: 1, max: 1200 },
+    min: { type: Number, min: 1, max: 1200, required: true },
+    max: { type: Number, min: 1, max: 1200, required: true },
   },
   authorship: {
     type: String,
     trim: true,
-    required: true,
     maxlength: 100,
   },
   createdBy: {
