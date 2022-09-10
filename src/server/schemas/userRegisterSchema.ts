723db9aded6b732ecf59be5f3fdfcf4a9aa8478a
Carlos <casantosmu@gmail.com>
2022-09-10 12:23:32 +0200

    Added min max on joi userRegisterSchema

--- src/server/schemas/userRegisterSchema.ts
+++ src/server/schemas/userRegisterSchema.ts
@@ -3,7 +3,7 @@ import { Joi } from "express-validation";
 const userRegisterSchema = {
   body: Joi.object({
     user: Joi.object({
-      name: Joi.string().required(),
+      name: Joi.string().required().min(3).max(20),
       email: Joi.string().required().min(3).max(50),
       password: Joi.string().required().min(6),
     }).required(),
