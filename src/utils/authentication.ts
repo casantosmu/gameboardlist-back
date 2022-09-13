6bb4e6ca52dffb3172eac719d2f4e8903fd6ff30
Carlos <casantosmu@gmail.com>
2022-09-13 20:14:51 +0200

    Added ES6 String Literals for secret dotenv

--- src/utils/authentication.ts
+++ src/utils/authentication.ts
@@ -3,10 +3,10 @@ import bcrypt from "bcrypt";
 import { UserPayload } from "../types/user";
 
 export const getToken = (payload: UserPayload) =>
-  jws.sign(payload, process.env.SECRET);
+  jws.sign(payload, `${process.env.SECRET}`);
 
 export const verifyToken = (token: string) =>
-  jws.verify(token, process.env.SECRET);
+  jws.verify(token, `${process.env.SECRET}`);
 
 export const getEncriptedData = (text: string, salt: number = 10) =>
   bcrypt.hash(text, salt);
