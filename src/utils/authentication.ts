6282c3dbfdd85e83c462393721d1951a26dde9d0
Carlos <casantosmu@gmail.com>
2022-09-13 20:18:16 +0200

    Added loadEnvironment for authentication

--- src/utils/authentication.ts
+++ src/utils/authentication.ts
@@ -1,12 +1,13 @@
+import "../loadEnvironment";
 import jws from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { UserPayload } from "../types/user";
 
 export const getToken = (payload: UserPayload) =>
-  jws.sign(payload, `${process.env.SECRET}`);
+  jws.sign(payload, process.env.SECRET);
 
 export const verifyToken = (token: string) =>
-  jws.verify(token, `${process.env.SECRET}`);
+  jws.verify(token, process.env.SECRET);
 
 export const getEncriptedData = (text: string, salt: number = 10) =>
   bcrypt.hash(text, salt);
