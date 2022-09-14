f78c373810f52b334746ed2ab9889f51c910b746
Carlos <casantosmu@gmail.com>
2022-09-14 18:47:48 +0200

    Added limit middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 import { generalError, notFoundError } from "./middlewares/errors/errors";
 import usersRouter from "./routers/usersRouter";
 import gameboardsRouter from "./routers/gameboardsRouter";
@@ -10,15 +11,21 @@ const HelmetOptions = {
   crossOriginResourcePolicy: false,
 };
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many request from this IP, please try again after an hour",
+});
+
 const app = express();
 
 app.use(express.json());
 app.use(helmet(HelmetOptions));
 app.use(cors());
 app.use(morgan("dev"));
+app.use(limiter);
 
 app.use("/uploads", express.static("uploads"));
-
 app.use("/users", usersRouter);
 app.use("/gameboards", gameboardsRouter);
 
