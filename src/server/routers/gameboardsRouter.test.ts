2a215e519a9018a13b43105b04d9d0a6f43619ee
Carlos <casantosmu@gmail.com>
2022-09-18 11:10:33 +0200

    Removed type any from newUser on gameboardRouter post endpoint test

--- src/server/routers/gameboardsRouter.test.ts
+++ src/server/routers/gameboardsRouter.test.ts
@@ -1,9 +1,10 @@
 import "../../test-utils/supertestSetup";
 import fs from "fs/promises";
 import request from "supertest";
+import { HydratedDocument } from "mongoose";
 import app from "..";
 import { fakeGameboard, fakeUser } from "../../test-utils/fakeData";
-import { UserPayload } from "../../types/user";
+import { User as IUser, UserPayload } from "../../types/user";
 import { getToken } from "../../utils/authentication";
 import "../../loadEnvironment";
 import User from "../../database/models/User";
@@ -25,7 +26,7 @@ jest.mock("@supabase/supabase-js", () => ({
 }));
 
 let authToken: string;
-let newUser: any;
+let newUser: HydratedDocument<IUser>;
 
 beforeAll(async () => {
   newUser = await User.create(fakeUser);
