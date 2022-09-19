b4acb253660edcde17246373a3d9eb23a3aff9aa
Carlos <casantosmu@gmail.com>
2022-09-19 17:54:09 +0200

    Modified user login and register shemas to require all fileds

--- src/server/schemas/userRegisterSchema.ts
+++ src/server/schemas/userRegisterSchema.ts
@@ -3,9 +3,9 @@ import { Joi } from "express-validation";
 const userRegisterSchema = {
   body: Joi.object({
     user: Joi.object({
-      name: Joi.string().min(3).max(20),
-      email: Joi.string().min(3).max(50),
-      password: Joi.string().min(6),
+      name: Joi.string().min(3).max(20).required(),
+      email: Joi.string().min(3).max(50).required(),
+      password: Joi.string().min(6).required(),
     }).required(),
   }),
 };
